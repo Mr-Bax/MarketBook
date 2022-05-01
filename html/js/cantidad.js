4c7708c4baef560fe56e5d43e0f7db76d0eedf4e
Mr-Bax <81442860+Mr-Bax@users.noreply.github.com>
2022-04-30 19:11:02 -0500

    Add files via upload

---
+++ html/js/cantidad.js
@@ -0,0 +1,12 @@
+var inicio = 0;
+function aumentar(){
+    var cantidad = document.getElementById('cantidad').value = ++inicio;
+}
+
+function disminuir(){
+    var cantidad = document.getElementById('cantidad').value = --inicio;
+}
+
+
+
+
